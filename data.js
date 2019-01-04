2f0b0ec1e7b43580a36fb23541da26a9faf92fd0
Hunter Metts <huntermettsdev@gmail.com>
2019-01-03 21:57:04 -0600

    MVP

--- data.js
+++ data.js
@@ -1,79 +1,189 @@
-const allCars = [
-    {
-        model: "Mustang",
-        color: "Midnight Blue",
-        year: 1976,
-        length: 120,
-        width: 62,
-        height: 47
-    },
-    {
-        model: "Xterra",
-        color: "Forest Green",
-        year: 2011,
-        length: 144,
-        width: 71,
-        height: 55
-    },
-    {
-        model: "Thunderbird",
-        color: "Red",
-        year: 2005,
-        length: 115,
-        width: 58,
-        height: 42
-    },
-    {
-        model: "Suburban",
-        color: "Grey",
-        year: 2015,
-        length: 149,
-        width: 73,
-        height: 58
-    }
-]
+// const allCars = [
+//     {
+//         model: "Mustang",
+//         color: "Midnight Blue",
+//         year: 1976,
+//         length: 120,
+//         width: 62,
+//         height: 47
+//     },
+//     {
+//         model: "Xterra",
+//         color: "Forest Green",
+//         year: 2011,
+//         length: 144,
+//         width: 71,
+//         height: 55
+//     },
+//     {
+//         model: "Thunderbird",
+//         color: "Red",
+//         year: 2005,
+//         length: 115,
+//         width: 58,
+//         height: 42
+//     },
+//     {
+//         model: "Suburban",
+//         color: "Grey",
+//         year: 2015,
+//         length: 149,
+//         width: 73,
+//         height: 58
+//     }
+// ]
 
-// Lightning Exercise 1: Create an object that represents a bill from your doctor's office. It should have the following properties: officeName, streetAddress, doctorName, patientName, visitDate, amountBilled, dueDate. You provide example values for each one.
-const dateVisited = "visitDate"
-const owed = "amountBilled"
-const patient = "patientName"
+// // Lightning Exercise 1: Create an object that represents a bill from your doctor's office. It should have the following properties: officeName, streetAddress, doctorName, patientName, visitDate, amountBilled, dueDate. You provide example values for each one.
+// const dateVisited = "visitDate"
+// const owed = "amountBilled"
+// const patient = "patientName"
 
 
-const doctorBill = [
-        {
-            officeName: "Reteef Skincare",
-            streetAddress: "123 Bluebell Way",
-            doctorName: "Lauren Reteef",
-            patientName: "Jordan Rosas",
-            visitDate: "1/3/19",
-            amountBilled: "$120",
-            dueDate:"1/21/19"
-        }
-    ]
+// const doctorBill = [
+//         {
+//             officeName: "Reteef Skincare",
+//             streetAddress: "123 Bluebell Way",
+//             doctorName: "Lauren Reteef",
+//             patientName: "Jordan Rosas",
+//             visitDate: "1/3/19",
+//             amountBilled: "$120",
+//             dueDate:"1/21/19"
+//         }
+//     ]
     
 
-    console.log(doctorBill[0][dateVisited])
-    console.log(doctorBill[0][owed])
-    console.log(doctorBill[0][patient])
+//     console.log(doctorBill[0][dateVisited])
+//     console.log(doctorBill[0][owed])
+//     console.log(doctorBill[0][patient])
    
 
-    const favFoods = [
+//     const favFoods = [
+//         {
+//             Food: "Pizza",
+//             Restaurant: "Dominoes",
+//             Type: "Carb",
+//             Cost: "$2.25 Per Slice"
+//         },
+//         {
+//             Food: "Grilled Chicken",
+//             Restaurant: "Home-made",
+//             Type: "Protein",
+//             Cost: "$5 Per lb"
+//         },
+//         {
+//             Food: "Sweet Potatoes",
+//             Restaurant: "Outback Steakhouse",
+//             Type: "Carb",
+//             Cost: "$2.99 Per potato"
+//         },
+//     ]
+
+
+// Practice:
+    const salesByWeek = [
         {
-            Food: "Pizza",
-            Restaurant: "Dominoes",
-            Type: "Carb",
-            Cost: "$2.25 Per Slice"
+            "vehicle": {
+                "year": 2013,
+                "model": "Tanex",
+                "make": "Tamp-dax",
+                "color": "quartz"
+            },
+            "sales_id": "782a23fd-8b83-4497-b9a1-41fd9b15648e",
+            "sales_agent": {
+                "mobile": "1-584-162-7444",
+                "last_name": "Larkin",
+                "first_name": "Tiara",
+                "email": "ready@gmail.com"
+            },
+            "purchase_date": "2017-06-07",
+            "gross_profit": 210,
+            "credit": {
+                "credit_provider": "United Services Automobile Assoc.",
+                "account": "491697193540559"
+            }
         },
         {
-            Food: "Grilled Chicken",
-            Restaurant: "Home-made",
-            Type: "Protein",
-            Cost: "$5 Per lb"
+            "vehicle": {
+                "year": 2012,
+                "model": "Volttanphase",
+                "make": "Tinlotis",
+                "color": "french fuchsia"
+            },
+            "sales_id": "c6775819-5d17-40db-94f4-00425db590ee",
+            "sales_agent": {
+                "mobile": "1-573-820-3780",
+                "last_name": "Schulist",
+                "first_name": "Vada",
+                "email": "davin@outlook.com"
+            },
+            "purchase_date": "2017-08-06",
+            "gross_profit": 1886.61,
+            "credit": {
+                "credit_provider": "TD Group US Holding",
+                "account": "494781657570"
+            }
         },
         {
-            Food: "Sweet Potatoes",
-            Restaurant: "Outback Steakhouse",
-            Type: "Carb",
-            Cost: "$2.99 Per potato"
+            "vehicle": {
+                "year": 2013,
+                "model": "Lexi-ton",
+                "make": "Ware-com",
+                "color": "metallic brown"
+            },
+            "sales_id": "d10631e7-24ca-414b-ac0f-34b286a30f14",
+            "sales_agent": {
+                "mobile": "1-356-831-5782",
+                "last_name": "Leannon",
+                "first_name": "Geovanni",
+                "email": "need@hotmail.com"
+            },
+            "purchase_date": "2017-12-27",
+            "gross_profit": 154.91,
+            "credit": {
+                "credit_provider": "State Street Corporation",
+                "account": "546417363501851"
+            }
         },
+        {
+            "vehicle": {
+                "year": 2011,
+                "model": "Onto-zun",
+                "make": "Strongzoom",
+                "color": "medium slate blue"
+            },
+            "sales_id": "586bf9d9-67c2-46c7-a6d1-de8484f6c474",
+            "sales_agent": {
+                "mobile": "774.020.0247",
+                "last_name": "Lang",
+                "first_name": "Jovani",
+                "email": "spell55@yahoo.com"
+            },
+            "purchase_date": "2017-03-05",
+            "gross_profit": 814.49,
+            "credit": {
+                "credit_provider": "M&T Bank Corporation",
+                "account": "34730046974022"
+            }
+        },
+        {
+            "vehicle": {
+                "year": 2014,
+                "model": "Blue-run",
+                "make": "Kon-flex",
+                "color": "lumber"
+            },
+            "sales_id": "a5358cb3-6d47-4adb-a232-d5c5d91e25ec",
+            "sales_agent": {
+                "mobile": "886.374.3898",
+                "last_name": "Feil",
+                "first_name": "Kory",
+                "email": "ducimus@outlook.com"
+            },
+            "purchase_date": "2017-02-08",
+            "gross_profit": 587.52,
+            "credit": {
+                "credit_provider": "Atlantic Bank",
+                "account": "601149379893233"
+            }
+        }
     ]
--- main.js
+++ main.js
@@ -1,68 +1,91 @@
 const outputElement = document.querySelector("#app")
 
-/*
-    Just get a reference to the first object since
-    they all have the same properties
-*/
-// const firstCar = allCars[0]
+// /*
+//     Just get a reference to the first object since
+//     they all have the same properties
+// */
+// // const firstCar = allCars[0]
 
-// // Now iterate its keys
-// outputElement.innerHTML += "<h1>Properties</h1>"
-// for (const key of Object.keys(allCars[0])) {
-//   outputElement.innerHTML += `<div>${key}</div>`
-// }
+// // // Now iterate its keys
+// // outputElement.innerHTML += "<h1>Properties</h1>"
+// // for (const key of Object.keys(allCars[0])) {
+// //   outputElement.innerHTML += `<div>${key}</div>`
+// // }
 
-// // Iterate the array of cars. Individual objects stored in `car`.
-// outputElement.innerHTML += "<h1>Car List</h1>"
-// allCars.forEach(car => {
+// // // Iterate the array of cars. Individual objects stored in `car`.
+// // outputElement.innerHTML += "<h1>Car List</h1>"
+// // allCars.forEach(car => {
 
+// //     // Iterate all of the values of the current car
+// //     for (const value of Object.values(car)) {
+// //       outputElement.innerHTML += `<div>${value}</div>`
+// //     }
+// // })
+
+
+
+
+// // Lighting
+// doctorBill.forEach(key => {
+//         // Iterate all of the values of the current car
+//         for (const value of Object.values(key)) {
+//           console.log(value);
+//         }
+//     })
+
+// doctorBill.forEach(value => {
 //     // Iterate all of the values of the current car
-//     for (const value of Object.values(car)) {
-//       outputElement.innerHTML += `<div>${value}</div>`
+//     for (const key of Object.keys(value)) {
+//         console.log(key);
 //     }
 // })
 
 
+// outputElement.innerHTML += `<h1>Bill Keys</h1>`
+// for (const key of Object.keys(doctorBill[0])) {
+//     outputElement.innerHTML += `<span>${key}</span> <br>`
+// }
+
+// outputElement.innerHTML += "<h1>Car List</h1>"
+
+// allCars.forEach(car => {
+//     outputElement.innerHTML += "<hr/>"
+
+//     for (const entry of Object.entries(car)) {
+//         outputElement.innerHTML += `<div>${entry[0]}: ${entry[1]}</div>`
+//     }
+// })
 
 
-// Lighting
-doctorBill.forEach(key => {
-        // Iterate all of the values of the current car
-        for (const value of Object.values(key)) {
-          console.log(value);
-        }
-    })
-
-doctorBill.forEach(value => {
-    // Iterate all of the values of the current car
-    for (const key of Object.keys(value)) {
-        console.log(key);
-    }
-})
+// // lightning FOOD
+// favFoods.forEach(food => {
+//     outputElement.innerHTML += "<hr/>"
+//     let stringOfHtml = '<article class="food">';
+//     for (const entry of Object.entries(food)) {
+//         stringOfHtml += `<div>${entry[0]}: ${entry[1]}<div>`
+//     }
+//     outputElement.innerHTML += stringOfHtml + "</article>"
+// })
 
 
-outputElement.innerHTML += `<h1>Bill Keys</h1>`
-for (const key of Object.keys(doctorBill[0])) {
-    outputElement.innerHTML += `<span>${key}</span> <br>`
-}
+// Practice from chapter
+outputElement.innerHTML += "<h1>Weekly Sales Report</h1>"
 
-outputElement.innerHTML += "<h1>Car List</h1>"
-
-allCars.forEach(car => {
+salesByWeek.forEach(sale => {
+    let agentFirst = sale.sales_agent.first_name
+    let agentLast = sale.sales_agent.last_name
+    let year = sale.vehicle.year
+    let model = sale.vehicle.model
+    let make = sale.vehicle.make
+    let color = sale.vehicle.color
+    let profit = sale.gross_profit
     outputElement.innerHTML += "<hr/>"
-
-    for (const entry of Object.entries(car)) {
-        outputElement.innerHTML += `<div>${entry[0]}: ${entry[1]}</div>`
-    }
-})
-
-
-// lightning FOOD
-favFoods.forEach(food => {
-    outputElement.innerHTML += "<hr/>"
-    let stringOfHtml = '<article class="food">';
-    for (const entry of Object.entries(food)) {
-        stringOfHtml += `<div>${entry[0]}: ${entry[1]}<div>`
-    }
-    outputElement.innerHTML += stringOfHtml + "</article>"
+    outputElement.innerHTML += 
+    `<h2>${agentFirst} ${agentLast}</h2>
+     <p>year: ${year}</p>
+     <p>model: ${model}</p>
+     <p>make: ${make}</p>
+     <p>color: ${color}</p>
+     <h3>Profit: $${profit}<h3>
+    `
 })
